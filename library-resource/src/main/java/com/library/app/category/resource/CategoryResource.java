80d1303b5c6ef3141a183447a29411fa872e8a76
Sasindu <sinharaavishka@gmail.com>
2020-05-21 20:09:22 +0530

    finshed category 7

---
+++ library-resource/src/main/java/com/library/app/category/resource/CategoryResource.java
@@ -0,0 +1,9 @@
+package com.library.app.category.resource;
+
+import com.library.app.category.services.CategoryServices;
+
+public class CategoryResource {
+
+    CategoryServices categoryServices;
+
+}
---
+++ library-resource/src/test/java/com/library/app/category/resource/CategoryResourceUTest.java
@@ -0,0 +1,45 @@
+package com.library.app.category.resource;
+
+import static com.library.app.commontests.category.CategoryForTestsRepository.*;
+import static com.library.app.commontests.utils.FileTestNameUtils.*;
+import static com.library.app.commontests.utils.JsonTestUtils.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import javax.ws.rs.core.Response;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.library.app.category.services.CategoryServices;
+
+public class CategoryResourceUTest {
+    private CategoryResource categoryResource;
+
+    private static final String PATH_RESOURCE = "categories";
+
+    @Mock
+    private CategoryServices categoryServices;
+
+    @Before
+    public void initTestCase() {
+        MockitoAnnotations.initMocks(this);
+        categoryResource = new CategoryResource();
+
+        categoryResource.categoryServices = categoryServices;
+    }
+
+    @Test
+    public void addValidCategory() {
+        when(categoryServices.add(java())).thenReturn(categoryWithId(java(), 1L));
+
+        final Response response = categoryResource.add(readJsonFile(getPathFileRequest(PATH_RESOURCE,
+                "newCategory.json")));
+        assertThat(response.getStatus(), is(equalTo(201)));
+        assertJsonMatchesExpectedJson(response.getEntity().toString(), "{\"id\": 1}");
+    }
+
+}
