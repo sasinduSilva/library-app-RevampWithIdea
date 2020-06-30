63362af5e497fe899cb36b61f1c5b70dfb74dbc0
Sasindu <sinharaavishka@gmail.com>
2020-06-30 06:16:08 +0530

    annotated the AuthorFilterExtractorFromUrlUtest.java class and started coding on it

--- library-resource/src/test/java/com/library/app/author/resource/AuthorFilterExtractorFromUrlUtest.java
+++ library-resource/src/test/java/com/library/app/author/resource/AuthorFilterExtractorFromUrlUtest.java
@@ -1,4 +1,34 @@
 package com.library.app.author.resource;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import javax.ws.rs.core.UriInfo;
+
 public class AuthorFilterExtractorFromUrlUtest {
+
+    @Mock
+    private UriInfo uriInfo;
+
+    @Before
+    public void initTestCase(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void OnlyDefaultValues(){
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private void setUpUriInfo(String page, String perPage, String name, String sort){
+        
+    }
+
+
+
+
 }
+
