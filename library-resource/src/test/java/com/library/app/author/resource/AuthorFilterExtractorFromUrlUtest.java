f3c44ebe51f76c5ed4e7a6828e35b28ee09625ec
Sasindu <sinharaavishka@gmail.com>
2020-06-30 06:24:24 +0530

    completed the setUpUriInfo method

--- library-resource/src/test/java/com/library/app/author/resource/AuthorFilterExtractorFromUrlUtest.java
+++ library-resource/src/test/java/com/library/app/author/resource/AuthorFilterExtractorFromUrlUtest.java
@@ -5,7 +5,13 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.UriInfo;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AuthorFilterExtractorFromUrlUtest {
 
@@ -24,7 +30,20 @@ public class AuthorFilterExtractorFromUrlUtest {
 
     @SuppressWarnings("unchecked")
     private void setUpUriInfo(String page, String perPage, String name, String sort){
-        
+        Map<String,String> parameters = new LinkedHashMap<>();
+        parameters.put("page",page);
+        parameters.put("per page",perPage);
+        parameters.put("name",name);
+        parameters.put("sort",sort);
+
+        MultivaluedMap<String, String> multiMap = mock(MultivaluedMap.class);
+
+        for (Map.Entry<String, String> keyValue: parameters.entrySet()) {
+            when(multiMap.getFirst(keyValue.getKey())).thenReturn(keyValue.getValue());
+
+        }
+
+        when(uriInfo.getQueryParameters()).thenReturn(multiMap);
     }
 
 
