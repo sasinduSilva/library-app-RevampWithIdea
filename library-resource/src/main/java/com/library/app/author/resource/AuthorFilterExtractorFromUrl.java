db36eaf7a624852aa0bcad5b2440f8125b5ed0fe
Sasindu <sinharaavishka@gmail.com>
2020-06-30 06:10:53 +0530

    created the constructor and the getFilter methods in AuthorFilterExtractorFromUrl.java class

--- library-resource/src/main/java/com/library/app/author/resource/AuthorFilterExtractorFromUrl.java
+++ library-resource/src/main/java/com/library/app/author/resource/AuthorFilterExtractorFromUrl.java
@@ -1,5 +1,20 @@
 package com.library.app.author.resource;
 
+import com.library.app.author.model.filter.AuthorFilter;
+
+import javax.ws.rs.core.UriInfo;
+
 public class AuthorFilterExtractorFromUrl {
-    
+    private UriInfo uriInfo;
+
+    public AuthorFilterExtractorFromUrl(UriInfo uriInfo) {
+        this.uriInfo= uriInfo;
+    }
+
+    public AuthorFilter getFilter(){
+        AuthorFilter authorFilter = new AuthorFilter();
+
+        return authorFilter;
+    }
+
 }
